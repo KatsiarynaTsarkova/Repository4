f81ee08643b528f1813d94fa93023d9045ec078f
Kate <katya_tsarkov@mail.ru>
2022-09-08 21:45:55 +0300

    Test1

---
+++ Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Number = new Random().Next(0,99999);
+Console.WriteLine(Number);
+string fivNum = Convert.ToString(Number);
+if (fivNum.Length==5)
+{
+    if(fivNum[0]==fivNum[4] || fivNum[1]==fivNum[3])
+    {
+       Console.WriteLine("Полиндром");
+    } 
+    else
+    {
+       Console.WriteLine("Не полиндром"); 
+    }
+}
+else
+{
+  Console.WriteLine("Число не является пятизначным");   
+}
+    
+    
+
